4352bd0d8169af577495d4aab5bab9a022ea192a
Burak <burakkarakoyiu@gmail.com>
2017-07-02 15:36:29 +0100

    GP-1: First Commit

---
+++ src/main/java/github/MainClass.java
@@ -0,0 +1,10 @@
+package main.java.github;
+
+/**
+ * Created by burak on 7/2/2017.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("GITHUB PROJECT");
+    }
+}
